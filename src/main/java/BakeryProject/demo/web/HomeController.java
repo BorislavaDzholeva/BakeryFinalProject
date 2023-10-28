a7799e08cf3c170e9edfaa0d3c4921eb66810ba2
Borislava Djoleva <borislavadzholeva@gmail.com>
2023-10-28 15:00:36 +0300

    Initial of home functionality

---
+++ src/main/java/BakeryProject/demo/web/HomeController.java
@@ -0,0 +1,26 @@
+package BakeryProject.demo.web;
+
+import BakeryProject.demo.models.entity.Product;
+import BakeryProject.demo.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    private final ProductService productService;
+
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/")
+    public String all(Model model) {
+        return "home";
+    }
+}
