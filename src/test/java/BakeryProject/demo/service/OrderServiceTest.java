a328eeff4c63051c38c8df90e76435d4f5a520fc
Borislava Djoleva <borislavadzholeva@gmail.com>
2023-11-29 13:26:48 +0200

    Refactoring OrderServiceTest.

--- src/test/java/BakeryProject/demo/service/OrderServiceTest.java
+++ src/test/java/BakeryProject/demo/service/OrderServiceTest.java
@@ -17,13 +17,10 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -36,9 +33,8 @@ public class OrderServiceTest {
     @Mock
     private CartItemRepository mockCartItemRepository;
     @Captor
-    private ArgumentCaptor<Order> orderArgumentCaptor;
+    private ArgumentCaptor<Long> argumentCaptor = ArgumentCaptor.forClass(Long.class);
     private Order testOrder;
-    private OrderItem testOrderItem;
     private UserEntity testUser;
     private Cart testCart;
     private CreateOrderDTO testCreateOrderDTO;
@@ -73,9 +69,6 @@ public class OrderServiceTest {
                 setId(1L);
                 setTotalPrice(BigDecimal.valueOf(1.0));
                 setOrderStatus(OrderStatusEnum.Pending);
-                setCity("city");
-                setAddress("address");
-                setPhoneNumber("phone number");
                 setUser(testUser);
                 setOrderItems(new ArrayList<>());
 
@@ -89,15 +82,7 @@ public class OrderServiceTest {
 
             }
         };
-        testOrderItem = new OrderItem() {
-            {
-                setId(1L);
-                setProduct(null);
-                setQuantity(1);
-                setPrice(BigDecimal.valueOf(1.0));
 
-            }
-        };
     }
 
 
@@ -122,17 +107,13 @@ public class OrderServiceTest {
 
     @Test
     public void testCreateOrder() {
-        // Arrange
-        String username = "testUser";
-        CreateOrderDTO createOrderDTO = new CreateOrderDTO();
-        createOrderDTO.setCity("city");
-        createOrderDTO.setAddress("address");
-        createOrderDTO.setPhoneNumber("phone number");
 
-        UserEntity user = new UserEntity();
-        Cart cart = new Cart();
+        String username = "testUser";
+
+        testUser = new UserEntity();
+        testCart = new Cart();
         CartItem cartItem = new CartItem();
-        cartItem.setId(1L); // Set an id for the CartItem
+        cartItem.setId(1L);
         Product product = new Product();
         product.setName("product 1");
         product.setPrice(BigDecimal.valueOf(10));
@@ -140,16 +121,12 @@ public class OrderServiceTest {
         cartItem.setQuantity(1);
         ArrayList<CartItem> cartItems = new ArrayList<>();
         cartItems.add(cartItem);
-        cart.setCartItems(cartItems);
-        user.setCart(cart);
+        testCart.setCartItems(cartItems);
+        testUser.setCart(testCart);
+        when(mockUserRepository.findByUsername(username)).thenReturn(Optional.of(testUser));
+        serviceToTest.createOrder(testCreateOrderDTO, username);
 
-        when(mockUserRepository.findByUsername(username)).thenReturn(Optional.of(user));
 
-        // Act
-        serviceToTest.createOrder(createOrderDTO, username);
-
-        // Assert
-        ArgumentCaptor<Long> argumentCaptor = ArgumentCaptor.forClass(Long.class);
         Mockito.verify(mockOrderRepository, Mockito.times(1)).save(any(Order.class));
         Mockito.verify(mockUserRepository, Mockito.times(1)).save(any(UserEntity.class));
         Mockito.verify(mockCartItemRepository, Mockito.times(1)).deleteById(argumentCaptor.capture());
@@ -157,39 +134,28 @@ public class OrderServiceTest {
     }
     @Test
     public void testCreateOrderWithNoUser() {
-        // Arrange
+
         String username = "testUser";
-        CreateOrderDTO createOrderDTO = new CreateOrderDTO();
-        createOrderDTO.setCity("city");
-        createOrderDTO.setAddress("address");
-        createOrderDTO.setPhoneNumber("phone number");
 
         when(mockUserRepository.findByUsername(username)).thenReturn(Optional.empty());
 
-        // Act & Assert
-        assertThrows(IllegalArgumentException.class, () -> serviceToTest.createOrder(createOrderDTO, username));
+        Assertions.assertThrows(IllegalArgumentException.class, ()
+                -> serviceToTest.createOrder(testCreateOrderDTO, username));
     }
 
     @Test
     public void testCreateOrderWithEmptyCart() {
-        // Arrange
+
         String username = "testUser";
-        CreateOrderDTO createOrderDTO = new CreateOrderDTO();
-        createOrderDTO.setCity("city");
-        createOrderDTO.setAddress("address");
-        createOrderDTO.setPhoneNumber("phone number");
 
-        UserEntity user = new UserEntity();
-        Cart cart = new Cart();
-        cart.setCartItems(new ArrayList<>());
-        user.setCart(cart);
+        testUser = new UserEntity();
+        testCart = new Cart();
+        testCart.setCartItems(new ArrayList<>());
+        testUser.setCart(testCart);
 
-        when(mockUserRepository.findByUsername(username)).thenReturn(Optional.of(user));
+        when(mockUserRepository.findByUsername(username)).thenReturn(Optional.of(testUser));
+        serviceToTest.createOrder(testCreateOrderDTO, username);
 
-        // Act
-        serviceToTest.createOrder(createOrderDTO, username);
-
-        // Assert
         Mockito.verify(mockOrderRepository, Mockito.times(1)).save(any(Order.class));
         Mockito.verify(mockUserRepository, Mockito.times(1)).save(any(UserEntity.class));
         Mockito.verify(mockCartItemRepository, Mockito.never()).deleteById(any(Long.class));
@@ -197,46 +163,38 @@ public class OrderServiceTest {
 
     @Test
     public void testChangeStatusFromPendingToShipped() throws IllegalAccessException {
-        // Arrange
+
         Order order = new Order();
         order.setOrderStatus(OrderStatusEnum.Pending);
         order.setUser(testUser);
 
-        // Act
         serviceToTest.changeStatus(order);
 
-        // Assert
         Mockito.verify(mockOrderRepository, Mockito.times(1)).save(order);
         Assertions.assertEquals(OrderStatusEnum.Shipped, order.getOrderStatus());
     }
 
     @Test
     public void testChangeStatusFromShipped() throws IllegalAccessException {
-        // Arrange
         Order order = new Order();
         order.setOrderStatus(OrderStatusEnum.Shipped);
         order.setUser(testUser);
 
         when(mockUserRepository.findById(testUser.getId())).thenReturn(Optional.of(testUser));
-
-        // Act
         serviceToTest.changeStatus(order);
 
-        // Assert
         Mockito.verify(mockUserRepository, Mockito.times(1)).findById(testUser.getId());
     }
 
     @Test
     public void testChangeStatusFromShippedButUserNotFound() {
-        // Arrange
+
         Order order = new Order();
         order.setOrderStatus(OrderStatusEnum.Shipped);
         order.setUser(testUser);
 
         when(mockUserRepository.findById(testUser.getId())).thenReturn(Optional.empty());
-
-        // Act & Assert
-        assertThrows(IllegalAccessException.class, () -> serviceToTest.changeStatus(order));
+        Assertions.assertThrows(IllegalAccessException.class, () -> serviceToTest.changeStatus(order));
     }
 
 
